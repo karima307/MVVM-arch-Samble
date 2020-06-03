7faec77330ff494e464865367dd81d24dccc7066
Karima <karimamohamed307@gmail.com>
2020-06-03 11:40:44 +0200

    fix error message

--- baseArch/src/main/java/com/nmg/baseinfrastructure/data/remote/ErrorHandler.kt
+++ baseArch/src/main/java/com/nmg/baseinfrastructure/data/remote/ErrorHandler.kt
@@ -20,26 +20,5 @@ interface ErrorHandler {
 
 
     @SuppressLint("LogNotTimber")
-    fun getHttpExceptionError(error: Throwable): String?{
-
-        Log.e("ErrorHandler" , error.message)
-
-        val context = App.appInstance.applicationContext
-
-        return if (error is SocketTimeoutException) {
-            context.getString(R.string.requestTimeOutError)
-        } else if (error is MalformedJsonException) {
-            context.getString(R.string.responseMalformedJson)
-        } else if (error is UnknownHostException) {
-            context.getString(R.string.unknownError)
-        } else if (error is ConnectException) {
-            context.getString(R.string.networkError)
-        } else if (error is IOException) {
-            context.getString(R.string.networkError)
-        } else if (error is HttpException) {
-            context.getString(R.string.unknownError)
-        } else {
-            context.getString(R.string.unknownError)
-        }
-    }
+    fun getHttpExceptionError(error: Throwable): String?
 }
